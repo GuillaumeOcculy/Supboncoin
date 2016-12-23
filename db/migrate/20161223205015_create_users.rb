f6f3d32050271c23b4c34f73a2a820373d8b83a8
Guillaume Occuly <occuly.guillaume@gmail.com>
2016-12-23 20:50:59 +0000

    Create User model

---
+++ db/migrate/20161223205015_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :username
+      t.string :password_digest
+      t.boolean :is_admin, default: false
+      t.string :phone
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+    add_index :users, :phone, unique: true
+  end
+end
